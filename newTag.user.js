401b6c0ef201a981fe46b30922866f2f1af9755b
rxs-michael <ruanxusong@thundersdata.com>
2020-07-08 22:36:02 +0800

    完成初版

---
+++ newTag.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         新标签页打开工具
+// @namespace    https://github.com/RuanXuSong/newTagScript
+// @version      1.0
+// @description  在任何页面都可以在新标签页打开链接，符合中国人习惯
+// @author       Ruan Xusong
+// @match        http://*/*
+// @match        https://*/*
+// @license      MIT
+// @grant        none
+// @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  $.noConflict(); // 兼容网页中$
+  // dom加载完成后，运行jquery函数域
+  jQuery(document).ready(function ($) {
+    $('a').attr('target', '_blank');
+  });
+})();
