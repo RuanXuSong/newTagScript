6113ea0434c66599b9d37ab86bbab6743717cfa4
rxs-michael <ruanxusong@thundersdata.com>
2020-07-09 09:52:36 +0800

    优化代码

--- newTag.user.js
+++ newTag.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         新标签页打开工具(newTagEverywhere)
 // @namespace    https://github.com/RuanXuSong/newTagScript
-// @version      1.0
+// @version      1.1
 // @description  在任何页面都可以在新标签页打开链接，符合中国人习惯
 // @author       Ruan Xusong
 // @match        http://*/*
@@ -12,10 +12,6 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
-  $.noConflict(); // 兼容网页中$
-  // dom加载完成后，运行jquery函数域
-  jQuery(document).ready(function ($) {
-    $('a').attr('target', '_blank');
-  });
+  const linkList = document.getElementsByTagName('a');
+  Array.from(linkList).forEach((item) => item.setAttribute('target', '_blank'));
 })();
